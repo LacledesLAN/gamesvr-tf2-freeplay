6289a401193d46d9ab4695173ae137e58adadfd2
James Dudley <jedudley@outlook.com>
2022-10-15 18:48:43 -0500

    add build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+docker pull lacledeslan/gamesvr-tf2:latest
+
+echo -e '\n\033[1m[Build tf2-freeplay]\033[0m'
+docker build . -f linux.Dockerfile --rm -t lacledeslan/gamesvr-tf2-freeplay:latest --build-arg BUILDNODE="$(cat /proc/sys/kernel/hostname)";
+docker run -it --rm lacledeslan/gamesvr-tf2-freeplay:latest ./ll-tests/gamesvr-tf2-freeplay.sh;
+docker push lacledeslan/gamesvr-tf2-freeplay:latest
